3f2e97ce9c117dbecdb8b479aa853bfca78d33b6
Aguadebebe <81484212+Aguadebebe@users.noreply.github.com>
2025-02-08 08:47:23 -0500

    deciding on cleanest way to write map function for text coloring

--- src/Components/MapColorChars.js
+++ src/Components/MapColorChars.js
@@ -3,16 +3,35 @@
 const MapColorChars = ({ text, colors }) => {
     return (
         <div> 
-           {text.split("").map((character, index) => (
+        {text.split("").map((character, index) => {
+            const color = colors[index % colors.length];
+            return (
+                <span key={index} style={{ color: color }}>
+                  {character}
+                </span>
+            );
+          })} 
+        </div>
+    );
+}
+
+export default MapColorChars;
+
+/** {text.split("").map((character, index) => {
+    const color = colors[index % colors.length];
+    return (
+        <span key={index} style={{ color: color }}>
+          {character}
+        <span>
+    );
+  })} 
+  */    
+ 
+  /** {text.split("").map((character, index) => (
              <span
               key={index}
               style={{ color: colors[index % colors.length] }}
              >
               {character}
              </span>
-          ))}
-        </div>
-    );
-}
-
-export default MapColorChars;
+          ))}*/
